d99eb68ec9e5be889667a55b64d873d271e7177c
shehan2006711 <154323632+shehan2006711@users.noreply.github.com>
2025-02-13 17:19:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Past Your Session Id 🍃",
+SESSION_ID: process.env.SESSION_ID || "𝐐𝐔𝐄𝐄𝐍-𝐑𝐀𝐒𝐇𝐔-𝐌𝐃=F5on1a7T#W871edBMMlURWIFm9ugVdVnLIQEJCfwwRksE3YZRRj8",
 //උඩ ☝️ තියෙන Past Sesion Id කියන එක අයින් කරලා ඔයාගෙ Sesion Id එක Past කරන්න 😁
-MONGODB: process.env.MONGODB || "Past Your MongoDB Url 🍃",
+MONGODB: process.env.MONGODB || "mongodb://mongo:dHFG-ef6CAFaGf5F1Gf-Cbg6AadbDcff@viaduct.proxy.rlwy.net:11588",
 //උඩ ☝️ තියෙන Past MongoDB Url කියන එක අයින් කරලා ඔයාගෙ MongoDB Url එක Past කරන්න 😁
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/BsjkCDP/9555.jpg",
 BOT_NAME: process.env.BOT_NAME || "QUEEN-RASHU-MD",
